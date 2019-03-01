8b98699e4784474194bbdb9853a24a8ab7d7d0db
catai9 <32720322+catai9@users.noreply.github.com>
2019-02-28 20:13:45 -0500

    Update homePopups.js

--- scripts/homePopups.js
+++ scripts/homePopups.js
@@ -4,7 +4,7 @@ $(function(){
     var $interest = $('.popupGrouping').find('li');
     var $popup = $interest.find('.popuptext');
     
-    $($interest).on('click', function(){
+    $($interest).on('click', function(event){
         $popup.removeClass("show");
         $(event.target).children().toggleClass("show");
     })
